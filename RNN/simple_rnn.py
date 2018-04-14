505cb5c4fbca9589e5cf9cad262d4b5e7c8f85dc
Maoan <maoan119@gmail.com>
2018-04-14 02:44:42 -0400

    Simple RNN model
    Used to predict the next word.

---
+++ RNN/simple_rnn.py
@@ -0,0 +1,42 @@
+import csv
+
+import itertools
+# import spacy
+import nltk
+#nltk.download()
+
+PATH='../datasets/acllmdb'
+
+TRN_PATH = 'train/all/'
+VAL_PATH = 'test/all/'
+TRN = f'{PATH}{TRN_PATH}'
+VAL = f'{PATH}{VAL_PATH}'
+
+vocabulary_size = 8000
+unknown_token = "UNKNOWN_TOKEN"
+sentence_start_token = "SENTENCE_START"
+sentence_end_token = "SENTENCE_END"
+
+# Read the data and append SENTENCE_START and SENTENCE_END tokens
+print("Reading CSV file...")
+with open('../datasets/reddit-comments.csv', 'r', encoding="utf-8") as f:
+    # Split full comments into sentences
+    reader = csv.reader(f, skipinitialspace=True)
+    next(reader)
+    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].lower()) for x in reader])
+    # Append SENTENCE_START and SENTENCE_END
+    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+print("Parsed %d sentences." % (len(sentences)))
+
+# Tokenize the sentences into words
+tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+# Count the word frequencies
+word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+print("Found %d unique words tokens." % len(word_freq.items()))
+
+# Get the most common words and build index_to_word and word_to_index vectors
+vocab = word_freq.most_common(vocabulary_size-1)
+index_to_word = [x[0] for x in vocab]
+index_to_word.append(unknown_token)
+word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
